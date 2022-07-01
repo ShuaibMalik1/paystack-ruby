4114baa2b3ba11293d3df553c1081f96ec6aeaa5
Shuaib Malik <shuaib.malik1@gmail.com>
2022-07-02 01:55:16 +0530

    Added Transfer Recipient APIs

--- lib/paystack/objects/recipients.rb
+++ lib/paystack/objects/recipients.rb
@@ -7,8 +7,20 @@ class PaystackRecipients < PaystackBaseObject
     return PaystackRecipients.create(@paystack, data)
   end
 
+  def get(id_or_code)
+    return PaystackRecipients.get(@paystack, id_or_code)
+  end
+
+  def update(id_or_code, data={})
+    return PaystackRecipients.update(@paystack, id_or_code,  data)
+  end
+
   def list(page=1)
     return PaystackRecipients.list(@paystack, page)
+  end
+
+  def delete(id_or_code)
+    return PaystackRecipients.delete(@paystack, id_or_code)
   end
 
 
@@ -16,11 +28,21 @@ class PaystackRecipients < PaystackBaseObject
     initPostRequest(paystackObj, "#{API::RECIPIENT_PATH}", data, true)
   end
 
+  def PaystackRecipients.get(paystackObj, id_or_code)
+    initGetRequest(paystackObj, "#{API::RECIPIENT_PATH}/#{id_or_code}",)
+  end
+
+  def PaystackRecipients.update(paystackObj, id_or_code, data={})
+    initPutRequest(paystackObj,"#{API::RECIPIENT_PATH}/#{id_or_code}",  data)
+  end
+
   def PaystackRecipients.list(paystackObj, page=1)
     initGetRequest(paystackObj, "#{API::RECIPIENT_PATH}/?page=#{page}")
   end
 
-
+  def PaystackRecipients.delete(paystackObj, id_or_code)
+    initDeleteRequest(paystackObj, "#{API::RECIPIENT_PATH}/#{id_or_code}",)
+  end
 
 
 end
